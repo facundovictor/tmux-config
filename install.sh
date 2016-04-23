be4bd4c5d683d06bd86ee1794ff6fb5f1f8ec7aa
Facundo Victor <facundovt@gmail.com>
2016-04-23 10:12:14 -0300

    Add powerline config installation

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create the dir where the tmux files will be saved
-mkdir -p /etc/tmux
+mkdir -p /etc/tmux/powerline
 
 # Copy the configuration code into the config directory
-cp ./etc/* /etc/tmux/
+cp -r ./etc/* /etc/tmux/
 
 # Create the simbolic link to reference the tmux configuration
 ln -s /etc/tmux/tmux.conf ~/.tmux.conf
