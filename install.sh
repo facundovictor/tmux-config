9cf7355ac329c9ffac426b234cc5134b8b05ca52
Facundo Victor <facundovt@gmail.com>
2016-04-26 17:11:37 -0300

    Add argument -l to generate links only

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# Create the dir where the tmux files will be saved
-mkdir -p /etc/tmux/conf/powerline
+# Variable for know if the installer must only generate the links.
+LINK_ONLY=false
 
-# Copy the configuration code into the config directory
-cp -r ./etc/* /etc/tmux/
+# Parse arguments
+while getopts ":l" opt; do
+    case $opt in
+        l)
+            LINK_ONLY=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
+if [ "$LINK_ONLY" = false ] ; then
+    # Create the dir where the tmux files will be saved
+    mkdir -p /etc/tmux/conf/powerline
+
+    # Copy the configuration code into the config directory
+    cp -r ./etc/* /etc/tmux/
+fi
 
 # Create the simbolic links to reference the tmux configuration
 ln -s /etc/tmux/tmux.conf ~/.tmux.conf
