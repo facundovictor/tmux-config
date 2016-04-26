ba889f26c27925453789d98e3f48ee5176bd2fc5
Facundo Victor <facundovt@gmail.com>
2016-04-26 16:08:02 -0300

    Addapt installer to new dir structure

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Create the dir where the tmux files will be saved
-mkdir -p /etc/tmux/powerline
+mkdir -p /etc/tmux/conf/powerline
 
 # Copy the configuration code into the config directory
 cp -r ./etc/* /etc/tmux/
 
-# Create the simbolic link to reference the tmux configuration
+# Create the simbolic links to reference the tmux configuration
 ln -s /etc/tmux/tmux.conf ~/.tmux.conf
+ln -s /etc/tmux/conf/ ~/.tmux
